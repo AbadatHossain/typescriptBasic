08b5e7c38e89776e22b1249123f5b92b27d48d5e
AbadatHossain <abadat26@gmail.com>
2024-03-19 20:30:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// let user : string;
+// user = "100";
+// function display(user: string | number){
+//     console.log(user)
+// }
+// display(10)
+var info;
+info = "ab";
+function displayinfo(info) {
+    console.log(info);
+}
+displayinfo("17");
---
+++ index.ts
@@ -0,0 +1,16 @@
+// let user : string;
+// user = "100";
+
+// function display(user: string | number){
+//     console.log(user)
+// }
+// display(10)
+
+
+let info : string;
+info = "ab"
+
+function displayinfo(info: string){
+    console.log(info)
+}
+displayinfo("17")
